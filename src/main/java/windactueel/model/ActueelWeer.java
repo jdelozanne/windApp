83718ed7048e02429e2bbffdf771048492aff47f
Julia de Lozanne <jdelozanne@gmail.com>
2018-03-19 15:51:31 +0100

    initial commit

---
+++ src/main/java/windactueel/model/ActueelWeer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package windactueel.model;
+
+/**
+ *
+ * @author julia
+ */
+public class ActueelWeer {
+    //hier de json api gebruiken om actueelweer te vullen met data
+}
---
+++ src/main/java/windactueel/service/WindService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package windactueel.service;
+
+/**
+ *
+ * @author julia
+ */
+public class WindService {
+    //hier gaan berekenen of de windkracht goed is of niet
+    
+    //hier gaan berekenen of de windrichting goed is of niet
+    
+}
